a744e317ba0456727454065a6ae1905a447a5873
lorenzo Tribuiani <s1082416@studenti.univpm.it>
2020-04-28 11:38:58 +0200

    miglioramento splash screen

--- Tailorin/app/src/main/java/com/lorenzotribuiani/tailorin/SplashScreen.java
+++ Tailorin/app/src/main/java/com/lorenzotribuiani/tailorin/SplashScreen.java
@@ -3,6 +3,8 @@ package com.lorenzotribuiani.tailorin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -23,9 +25,11 @@ public class SplashScreen extends AppCompatActivity {
         logo = findViewById(R.id.Logo);
         text = findViewById(R.id.Text);
 
+        //animazioni del fade iniziale
         text.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_n_translate_bottom));
         logo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_n_translate_top));
 
+        //ottengo l'animazione di rimbalzo
         final Animation bow = AnimationBow(logo);
 
         logo.postDelayed(new Runnable() {
@@ -39,15 +43,18 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(new Intent(SplashScreen.this, HomePage.class));
+                //cambio il tipo di transizione fra le activity dallo scorrimento al fade (solo in questo caso)
                 Animatoo.animateFade(SplashScreen.this);
             }
         }, 2000);
     }
 
 
-    private Animation AnimationBow(final View target1){
+    private Animation AnimationBow(final ImageView target1){
+        //ottengo la prima animazione che rimpicciolisce il logo
         Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.resize_small);
 
+        //quando la prima termina inizia la seconda
         animation.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -56,6 +63,10 @@ public class SplashScreen extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
+                //imposto come drawable il logo animato
+                target1.setImageDrawable(getDrawable(R.drawable.animated_logo_no_text));
+                //faccio partire le animazioni della view e dell'immagine
+                ((AnimatedVectorDrawable)target1.getDrawable()).start();
                 target1.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.resize_big));
             }
 
