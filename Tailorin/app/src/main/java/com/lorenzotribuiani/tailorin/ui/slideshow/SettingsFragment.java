4135ab3f91aeefa1ff0458cc1e7f265c8c67093c
lorenzo Tribuiani <s1082416@studenti.univpm.it>
2020-05-05 18:55:30 +0200

    sistemato side menu

--- Tailorin/app/src/main/java/com/lorenzotribuiani/tailorin/ui/slideshow/SettingsFragment.java
+++ Tailorin/app/src/main/java/com/lorenzotribuiani/tailorin/ui/slideshow/SettingsFragment.java
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.lorenzotribuiani.tailorin.R;
 
-public class SlideshowFragment extends Fragment {
+public class SettingsFragment extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
+    private SettingsViewModel settingsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+        settingsViewModel =
+                ViewModelProviders.of(this).get(SettingsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
         final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
